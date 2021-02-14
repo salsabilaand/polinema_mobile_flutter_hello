8bcefad498c5faa923f779b614cbee5a1a9372ec
= <=>
2021-02-14 22:29:49 +0700

    edit title

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Tugas Pertama Flutter'),
+      home: MyHomePage(title: 'Tugas Pertama Flutter Salsabila'),
     );
   }
 }
